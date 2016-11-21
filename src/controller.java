16f2e7d84fe26e238d853304239bc8c52ba96510
adlerpoland <resepoland@o2.pl>
2016-11-21 23:01:47 +0100

    hsql implementacja

--- src/controller.java
+++ src/controller.java
@@ -1,4 +1,86 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import org.hsqldb.Statement;
 
 public class controller {
-
+	static void start() throws IOException, InterruptedException{
+		boolean con = connect_database();
+		
+		view.print_menu();
+		int i = read_choice();
+		if(choice_check(i)){
+			execute_choice(i);
+		}
+		else
+		{
+			Thread.sleep(1000);
+			start();
+		}
+	}
+	
+	static boolean choice_check(int i){
+		if(i==0) return false;
+		else return true;
+	}
+	
+	static void execute_choice(int i){
+		
+	}
+	
+	static int read_choice() throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		view.print_out("Wybierz dzialanie: ");
+        try{
+            int i = Integer.parseInt(br.readLine());
+            return i;
+        }catch(NumberFormatException nfe){
+            System.err.println("Nieprawidlowy wybor!");
+            return 0;
+        }
+	}
+	
+	public static boolean connect_database(){
+		final String DRIVER = "org.hsqldb.jdbcDriver";
+	    final String DB_URL = "jdbc:hsqldb:file:mydb;ifexists=false;hsqldb.lock_file=false";
+		
+	    Connection conn;
+	    java.sql.Statement stmt = null;
+	    
+	    //Check JDBC
+	    try 
+	    {
+			Class.forName(DRIVER);
+		} 
+	    catch (ClassNotFoundException e) 
+	    {
+			System.err.println("Brak sterownika JDBC");
+			e.printStackTrace();
+		}
+	    
+	    //Connect
+	    try {
+			   conn = DriverManager.getConnection(DB_URL,"admin","");   
+			   stmt = conn.createStatement();
+		} catch (SQLException e) {
+					System.err.println("Problem z otwarciem po³¹czenia");
+					e.printStackTrace();
+		}
+	    
+	  //ADRES
+	  		String createDataAdres="CREATE TABLE IF NOT EXISTS Adres(id INTEGER PRIMARY KEY IDENTITY, ulica VARCHAR(30), numer INTEGER, miejscowosc VARCHAR(50),  kod VARCHAR(6), kraj VARCHAR(40))";
+	  		try {
+	  			stmt.execute(createDataAdres);
+	  		} catch (SQLException e) {
+	  			System.err.println("Blad przy tworzeniu tabeli Adres");
+	  			e.printStackTrace();
+	  			return false;
+	  		}
+	    
+	    return true;
+	}
 }
--- src/start.java
+++ src/start.java
@@ -1,27 +1,24 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import org.hsqldb.Server;
 
 public class start {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, InterruptedException, SQLException {
 		// TODO Auto-generated method stub
-		view.print_out("Stra¿ Po¿arna 2016\n");
-		view.print_out("__________________\n");
 		
-		view.print_out("1. Dodaj dane\n");
-		view.print_out("2. Wyswietl tabele\n");
-		view.print_out("3. Zmodyfikuj dane\n");
-		view.print_out("4. Usun dane\n");
-		view.print_out(" \n");
+  
+		//Connect or create and connect to hsql
+		//Connection c = DriverManager.getConnection(DB_URL, "admin", "");
 		
-		 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		view.print_out("Wybierz dzialanie: ");
-        try{
-            int i = Integer.parseInt(br.readLine());
-        }catch(NumberFormatException nfe){
-            System.err.println("Nieprawidlowy wybor!");
-        }
+		//System.out.println(c);
+		
+		
+		controller.start();
+		
 	}
 
 }
